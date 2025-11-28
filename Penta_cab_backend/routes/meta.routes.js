706bda18707c6df2045e5ac80c40e5ec121b5b0f
pentacab <info.pentacab@gmail.com>
2025-11-28 12:47:16 +0530

    Delete Penta_cab_backend directory

--- Penta_cab_backend/routes/meta.routes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const airports = require('../data/airports');
-const cities = require('../cities.json');
-
-router.get('/api/airports', (req, res) => res.json(airports));
-router.get('/api/cities',  (req, res) => res.json(cities));
-
-module.exports = router;
--- Penta_cab_backend/routes/payments.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const paymentController = require("../controllers/payment.controller");
-
-router.post("/api/create-order", paymentController.createOrder);
-router.post("/api/verify-payment", paymentController.verifyPayment);
-module.exports = router;
